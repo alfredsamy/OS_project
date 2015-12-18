17fd7678f6cfd56c356f783965e37efdd9bc2a44
alfredsamy <alfred_samy2010@yahoo.com>
2015-12-18 20:06:50 +0200

    format

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -4,44 +4,38 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
-static void syscall_handler (struct intr_frame *);
+static void syscall_handler(struct intr_frame *);
 
-void
-syscall_init (void) 
-{
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+void syscall_init(void) {
+	intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
-{
-  printf ("system call!\n");
-  thread_exit ();
+static void syscall_handler(struct intr_frame *f UNUSED) {
+	printf("system call!\n");
+	thread_exit();
 }
 
 /* Reads a byte at user virtual address UADDR.
-   UADDR must be below PHYS_BASE.
-   Returns the byte value if successful, -1 if a segfault
-   occurred. */
-static int get_user (const uint8_t *uaddr)
-{
-  if(!is_user_vaddr(uaddr))
-    return -1;
-  int result;
-  asm ("movl $1f, %0; movzbl %1, %0; 1:"
-       : "=&a" (result) : "m" (*uaddr));
-  return result;
+ UADDR must be below PHYS_BASE.
+ Returns the byte value if successful, -1 if a segfault
+ occurred. */
+static int get_user(const uint8_t *uaddr) {
+	if (!is_user_vaddr(uaddr))
+		return -1;
+	int result;
+	asm ("movl $1f, %0; movzbl %1, %0; 1:"
+			: "=&a" (result) : "m" (*uaddr));
+	return result;
 }
 
 /* Writes BYTE to user address UDST.
-   UDST must be below PHYS_BASE.
-   Returns true if successful, false if a segfault occurred. */
-static bool put_user (uint8_t *udst, uint8_t byte)
-{
-  if(!is_user_vaddr(udst))
-    return false;
-  int error_code;
-  asm ("movl $1f, %0; movb %b2, %1; 1:"
-       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
-  return error_code != -1;
+ UDST must be below PHYS_BASE.
+ Returns true if successful, false if a segfault occurred. */
+static bool put_user(uint8_t *udst, uint8_t byte) {
+	if (!is_user_vaddr(udst))
+		return false;
+	int error_code;
+	asm ("movl $1f, %0; movb %b2, %1; 1:"
+			: "=&a" (error_code), "=m" (*udst) : "q" (byte));
+	return error_code != -1;
 }
