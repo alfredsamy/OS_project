62cd2fa4d196dca9231df4b7388e5396defa7055
alfredsamy <alfred_samy2010@yahoo.com>
2015-12-18 19:33:39 +0200

    project 2

--- threads/synch.c
+++ threads/synch.c
@@ -4,19 +4,16 @@
 
 /* Copyright (c) 1992-1996 The Regents of the University of California.
  All rights reserved.
-
  Permission to use, copy, modify, and distribute this software
  and its documentation for any purpose, without fee, and
  without written agreement is hereby granted, provided that the
  above copyright notice and the following two paragraphs appear
  in all copies of this software.
-
  IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO
  ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
  CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE
  AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF CALIFORNIA
  HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
  THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -35,12 +32,11 @@
 /* Initializes semaphore SEMA to VALUE.  A semaphore is a
  nonnegative integer along with two atomic operators for
  manipulating it:
-
  - down or "P": wait for the value to become positive, then
  decrement it.
-
  - up or "V": increment the value (and wake up one waiting
  thread, if any). */
+
 void sema_init(struct semaphore *sema, unsigned value) {
 	ASSERT(sema != NULL);
 
@@ -50,7 +46,6 @@ void sema_init(struct semaphore *sema, unsigned value) {
 
 /* Down or "P" operation on a semaphore.  Waits for SEMA's value
  to become positive and then atomically decrements it.
-
  This function may sleep, so it must not be called within an
  interrupt handler.  This function may be called with
  interrupts disabled, but if it sleeps then the next scheduled
@@ -75,7 +70,6 @@ void sema_down(struct semaphore *sema) {
 /* Down or "P" operation on a semaphore, but only if the
  semaphore is not already 0.  Returns true if the semaphore is
  decremented, false otherwise.
-
  This function may be called from an interrupt handler. */bool sema_try_down(
 		struct semaphore *sema) {
 	enum intr_level old_level;
@@ -97,7 +91,6 @@ void sema_down(struct semaphore *sema) {
 
 /* Up or "V" operation on a semaphore.  Increments SEMA's value
  and wakes up one thread of those waiting for SEMA, if any.
-
  This function may be called from an interrupt handler. */
 void sema_up(struct semaphore *sema) {
 	enum intr_level old_level;
@@ -106,9 +99,9 @@ void sema_up(struct semaphore *sema) {
 
 	old_level = intr_disable();
 	if (!list_empty(&sema->waiters)) {
-		//printf("******%s\n",list_entry (list_front (&sema->waiters),struct thread, elem)->name);
-		thread_unblock(
-				list_entry (list_pop_front (&sema->waiters),struct thread, elem));
+		struct list_elem *e = list_max(&sema->waiters, less, NULL);
+		list_remove(e);
+		thread_unblock(list_entry (e, struct thread, elem));
 	}
 	sema->value++;
 	intr_set_level(old_level);
@@ -149,7 +142,6 @@ static void sema_test_helper(void *sema_) {
  thread at any given time.  Our locks are not "recursive", that
  is, it is an error for the thread currently holding a lock to
  try to acquire that lock.
-
  A lock is a specialization of a semaphore with an initial
  value of 1.  The difference between a lock and such a
  semaphore is twofold.  First, a semaphore can have a value
@@ -160,17 +152,18 @@ static void sema_test_helper(void *sema_) {
  acquire and release it.  When these restrictions prove
  onerous, it's a good sign that a semaphore should be used,
  instead of a lock. */
+//int LOCK_ID;
 void lock_init(struct lock *lock) {
 	ASSERT(lock != NULL);
 
 	lock->holder = NULL;
+	lock->lock_id = LOCK_ID++;
 	sema_init(&lock->semaphore, 1);
 }
 
 /* Acquires LOCK, sleeping until it becomes available if
  necessary.  The lock must not already be held by the current
  thread.
-
  This function may sleep, so it must not be called within an
  interrupt handler.  This function may be called with
  interrupts disabled, but interrupts will be turned back on if
@@ -181,18 +174,21 @@ void lock_acquire(struct lock *lock) {
 	ASSERT(!lock_held_by_current_thread (lock));
 
 	if (lock->holder != NULL
-			&& lock->holder->priority < thread_get_priority()) {
-		if (lock->holder->donate < thread_get_priority())
-			donate(lock->holder, thread_get_priority());
+			&& lock->holder->initial_priority < thread_get_priority()) {
+		thread_current()->i = lock->lock_id;
+		thread_current()->lock_holder = lock->holder;
+		thread_donate_priority(thread_current(), lock->holder, lock->lock_id);
 	}
+
 	sema_down(&lock->semaphore);
 	lock->holder = thread_current();
+	thread_current()->i = -1;
+	thread_current()->lock_holder = NULL;
 }
 
 /* Tries to acquires LOCK and returns true if successful or false
  on failure.  The lock must not already be held by the current
  thread.
-
  This function will not sleep, so it may be called within an
  interrupt handler. */bool lock_try_acquire(struct lock *lock) {
 	bool success;
@@ -207,7 +203,6 @@ void lock_acquire(struct lock *lock) {
 }
 
 /* Releases LOCK, which must be owned by the current thread.
-
  An interrupt handler cannot acquire a lock, so it does not
  make sense to try to release a lock within an interrupt
  handler. */
@@ -215,8 +210,8 @@ void lock_release(struct lock *lock) {
 	ASSERT(lock != NULL);
 	ASSERT(lock_held_by_current_thread (lock));
 
+	thread_reset_priority(thread_current(), lock->lock_id);
 	lock->holder = NULL;
-	thread_reset_priority();
 	sema_up(&lock->semaphore);
 }
 
@@ -244,22 +239,30 @@ void cond_init(struct condition *cond) {
 	list_init(&cond->waiters);
 }
 
+/*true if a.priority < b.priority */
+bool cond_less(const struct list_elem *a,
+		const struct list_elem *b, void *aux) {
+	struct semaphore t1 = list_entry (a, struct semaphore_elem,elem)->semaphore;
+	struct semaphore t2 = list_entry (b, struct semaphore_elem, elem)->semaphore;
+	if (t1.cond_priority < t2.cond_priority)
+		return true;
+	else
+		return false;
+}
+
 /* Atomically releases LOCK and waits for COND to be signaled by
  some other piece of code.  After COND is signaled, LOCK is
  reacquired before returning.  LOCK must be held before calling
  this function.
-
  The monitor implemented by this function is "Mesa" style, not
  "Hoare" style, that is, sending and receiving a signal are not
  an atomic operation.  Thus, typically the caller must recheck
  the condition after the wait completes and, if necessary, wait
  again.
-
  A given condition variable is associated with only a single
  lock, but one lock may be associated with any number of
  condition variables.  That is, there is a one-to-many mapping
  from locks to condition variables.
-
  This function may sleep, so it must not be called within an
  interrupt handler.  This function may be called with
  interrupts disabled, but interrupts will be turned back on if
@@ -273,6 +276,7 @@ void cond_wait(struct condition *cond, struct lock *lock) {
 	ASSERT(lock_held_by_current_thread (lock));
 
 	sema_init(&waiter.semaphore, 0);
+	waiter.semaphore.cond_priority = thread_get_priority();
 	list_push_back(&cond->waiters, &waiter.elem);
 	lock_release(lock);
 	sema_down(&waiter.semaphore);
@@ -282,7 +286,6 @@ void cond_wait(struct condition *cond, struct lock *lock) {
 /* If any threads are waiting on COND (protected by LOCK), then
  this function signals one of them to wake up from its wait.
  LOCK must be held before calling this function.
-
  An interrupt handler cannot acquire a lock, so it does not
  make sense to try to signal a condition variable within an
  interrupt handler. */
@@ -292,14 +295,16 @@ void cond_signal(struct condition *cond, struct lock *lock UNUSED) {
 	ASSERT(!intr_context ());
 	ASSERT(lock_held_by_current_thread (lock));
 
-	if (!list_empty(&cond->waiters))
-		sema_up(&list_entry (list_pop_front (&cond->waiters),
-				struct semaphore_elem, elem)->semaphore);
+	if (!list_empty(&cond->waiters)) {
+		struct list_elem *e = list_max(&cond->waiters, cond_less, NULL);
+		list_remove(e);
+
+		sema_up(&list_entry (e,	struct semaphore_elem, elem)->semaphore);
+	}
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
  LOCK).  LOCK must be held before calling this function.
-
  An interrupt handler cannot acquire a lock, so it does not
  make sense to try to signal a condition variable within an
  interrupt handler. */
--- threads/thread.h
+++ threads/thread.h
@@ -7,16 +7,15 @@
 #include "threads/synch.h"
 
 /* States in a thread's life cycle. */
-enum thread_status
-  {
-    THREAD_RUNNING,     /* Running thread. */
-    THREAD_READY,       /* Not running but ready to run. */
-    THREAD_BLOCKED,     /* Waiting for an event to trigger. */
-    THREAD_DYING        /* About to be destroyed. */
-  };
+enum thread_status {
+	THREAD_RUNNING, /* Running thread. */
+	THREAD_READY, /* Not running but ready to run. */
+	THREAD_BLOCKED, /* Waiting for an event to trigger. */
+	THREAD_DYING /* About to be destroyed. */
+};
 
 /* Thread identifier type.
-   You can redefine this to whatever type you like. */
+ You can redefine this to whatever type you like. */
 typedef int tid_t;
 #define TID_ERROR ((tid_t) -1)          /* Error value for tid_t. */
 
@@ -24,15 +23,32 @@ typedef int tid_t;
 #define PRI_MIN 0                       /* Lowest priority. */
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
+#define NICE_MIN -20                    /* Lowest nice */
+#define NICE_MAX 20                     /* Highest nice */
+#define NICE_DEFAULT 0                  /* Default nice */
+#define LOAD_AVG_DEFAULT 0              /* Default load_avg */
+#define RECENT_CPU_DEFAULT 0            /* Default cpu reset */
+#define fixed_point_p 17                /* p for fixed point operations */
+#define fixed_point_q 14                /* q for fixed point operations */
+#define fixed_point_f (1<<fixed_point_q)  /* f for fixed point operations */
+#define int_to_fixed_point(N) (N*fixed_point_f)
+#define fp_to_int_round_zero(X) (X/fixed_point_f)
+#define fp_to_int_round_nearest(X) (X >= 0) ? ((X + (fixed_point_f / 2)) / fixed_point_f): ((X - (fixed_point_f / 2)) / fixed_point_f)
+#define add_fixed_point(X,Y) (X+Y)
+#define sub_y_from_x(X,Y) (X-Y)
+#define add_x_and_n(X,N) (X+(N*fixed_point_f))
+#define sub_n_from_x(X,N) (X-(N*fixed_point_f))
+#define fixed_point_multiply(X,Y) ((((int64_t) X) * Y) / fixed_point_f)
+#define mul_x_n(X,N) (X*N)
+#define fixed_point_div(X,Y) ((((int64_t) X)*fixed_point_f)/Y)
+#define div_x_n(X,N) (X/N)
 
 /* A kernel thread or user process.
-
    Each thread structure is stored in its own 4 kB page.  The
    thread structure itself sits at the very bottom of the page
    (at offset 0).  The rest of the page is reserved for the
    thread's kernel stack, which grows downward from the top of
    the page (at offset 4 kB).  Here's an illustration:
-
         4 kB +---------------------------------+
              |          kernel stack           |
              |                |                |
@@ -54,96 +70,110 @@ typedef int tid_t;
              |               name              |
              |              status             |
         0 kB +---------------------------------+
-
    The upshot of this is twofold:
-
       1. First, `struct thread' must not be allowed to grow too
          big.  If it does, then there will not be enough room for
          the kernel stack.  Our base `struct thread' is only a
          few bytes in size.  It probably should stay well under 1
          kB.
-
       2. Second, kernel stacks must not be allowed to grow too
          large.  If a stack overflows, it will corrupt the thread
          state.  Thus, kernel functions should not allocate large
          structures or arrays as non-static local variables.  Use
          dynamic allocation with malloc() or palloc_get_page()
          instead.
-
    The first symptom of either of these problems will probably be
    an assertion failure in thread_current(), which checks that
    the `magic' member of the running thread's `struct thread' is
    set to THREAD_MAGIC.  Stack overflow will normally change this
    value, triggering the assertion. */
-/* The `elem' member has a dual purpose.  It can be an element in
-   the run queue (thread.c), or it can be an element in a
-   semaphore wait list (synch.c).  It can be used these two ways
-   only because they are mutually exclusive: only a thread in the
-   ready state is on the run queue, whereas only a thread in the
-   blocked state is on a semaphore wait list. */
-struct thread
-  {
-    /* Owned by thread.c. */
-    tid_t tid;                          /* Thread identifier. */
-    enum thread_status status;          /* Thread state. */
-    char name[16];                      /* Name (for debugging purposes). */
-    uint8_t *stack;                     /* Saved stack pointer. */
-    int priority;                       /* Priority. */
-    int initial_priority;
-    int donate;
-    struct list_elem allelem;           /* List element for all threads list. */
 
-    /* Shared between thread.c and synch.c. */
-    struct list_elem elem;              /* List element. */
+/* The `elem' member has a dual purpose.  It can be an element in
+ the run queue (thread.c), or it can be an element in a
+ semaphore wait list (synch.c).  It can be used these two ways
+ only because they are mutually exclusive: only a thread in the
+ ready state is on the run queue, whereas only a thread in the
+ blocked state is on a semaphore wait list. */
+struct thread {
+	/* Owned by thread.c. */
+	tid_t tid; /* Thread identifier. */
+	enum thread_status status; /* Thread state. */
+	char name[16]; /* Name (for debugging purposes). */
+	uint8_t *stack; /* Saved stack pointer. */
+	int priority; /* Priority. */
+	int initial_priority;
+	struct thread *lock_holder;
+	int donate[100];
+	int i;
+	struct list_elem allelem; /* List element for all threads list. */
+	int nice;                   /* nice value */
+	int recent_cpu;             /* recent cpu value */
+
+	/* Shared between thread.c and synch.c. */
+	struct list_elem elem; /* List element. */
 
 #ifdef USERPROG
-    /* Owned by userprog/process.c. */
-    uint32_t *pagedir;                  /* Page directory. */
+	/* Owned by userprog/process.c. */
+	uint32_t *pagedir; /* Page directory. */
 #endif
 
     /* Owned by thread.c. */
     unsigned magic;                     /* Detects stack overflow. */
+
+
+	struct list_elem sleep_elem;
+	struct list_elem ready_elem;
+	int64_t time_put_to_sleep;
+	int64_t ticks_to_remain_sleep;
   };
 
+
 /* If false (default), use round-robin scheduler.
-   If true, use multi-level feedback queue scheduler.
-   Controlled by kernel command-line option "-o mlfqs". */
+ If true, use multi-level feedback queue scheduler.
+ Controlled by kernel command-line option "-o mlfqs". */
 extern bool thread_mlfqs;
 
-void thread_init (void);
-void thread_start (void);
+void thread_init(void);
+void thread_start(void);
 
-void thread_tick (void);
-void thread_print_stats (void);
+void thread_tick(void);
+void thread_print_stats(void);
 
-typedef void thread_func (void *aux);
-tid_t thread_create (const char *name, int priority, thread_func *, void *);
+typedef void thread_func(void *aux);
+tid_t thread_create(const char *name, int priority, thread_func *, void *);
 
-void thread_block (void);
-void thread_unblock (struct thread *);
+void thread_block(void);
+void thread_unblock(struct thread *);
 
-struct thread *thread_current (void);
-tid_t thread_tid (void);
-const char *thread_name (void);
+struct thread *thread_current(void);
+tid_t thread_tid(void);
+const char *thread_name(void);
 
-void thread_exit (void) NO_RETURN;
-void thread_yield (void);
+void thread_exit(void) NO_RETURN;
+void thread_yield(void);
 
 /* Performs some operation on thread t, given auxiliary data AUX. */
-typedef void thread_action_func (struct thread *t, void *aux);
-void thread_foreach (thread_action_func *, void *);
+typedef void thread_action_func(struct thread *t, void *aux);
+void thread_foreach(thread_action_func *, void *);
 
-int thread_get_priority (void);
-void thread_set_priority (int);
+int thread_get_priority(void);
+void thread_set_priority(int);
 
-int thread_get_nice (void);
-void thread_set_nice (int);
-int thread_get_recent_cpu (void);
-int thread_get_load_avg (void);
+int thread_get_nice(void);
+void thread_set_nice(int);
+int thread_get_recent_cpu(void);
+int thread_get_load_avg(void);
+void thread_calculate_priority(struct thread *t);
+void thread_calculate_recent_cpu(struct thread *t);
+void calculate_load_avg(void);
 
-bool less(const struct list_elem *a,const struct list_elem *b,void *aux);
-bool more(const struct list_elem *a,const struct list_elem *b,void *aux);
-void donate (struct thread *t, int d);
-void thread_reset_priority();
-void thread_donate_priority (int new_priority);
+bool less(const struct list_elem *a, const struct list_elem *b, void *aux);
+bool more(const struct list_elem *a, const struct list_elem *b, void *aux);
+void thread_reset_priority(struct thread *t, int lock_id);
+
+// returns true if wakeuptime of a is less than wake up time of b
+bool lessWakeupTime(const struct list_elem *a, const struct list_elem *b, void *aux);
+
+void thread_donate_priority(struct thread *donor, struct thread *t, int lock_id);
+int max_donation(struct thread *t);
 #endif /* threads/thread.h */
